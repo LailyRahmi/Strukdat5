e112523010b31afd809ba4a2497974ec624ebfc4
LailyAuliaRahmi <lailyauliarahmi@webmail.umm.ac.id>
2023-05-26 08:07:24 +0700

    latihan modul5

---
+++ src/latihanmodul5/Node.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package latihanmodul5;
+
+/**
+ *
+ * @author GF63
+ */
+public class Node {
+    int data;
+    Node left;
+    Node right;
+    
+    public Node(int data){
+        this.data = data;
+    }
+   
+}
